f387e3b7a52ba28ffe3936413b7bd89c73def188
rhonvyramos <rhonvyramos@gmail.com>
2023-10-01 09:35:51 -0400

    installing PWA logic implemented

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,7 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('POST into database')
 
@@ -31,7 +30,6 @@ export const putDb = async (content) => {
   console.log("content posted into database!", result)
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log("GET from database");
 
