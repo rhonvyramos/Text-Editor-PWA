5d63697f419ef08229d76ebae0ffc2c63c80ea5a
rhonvyramos <rhonvyramos@gmail.com>
2023-10-01 09:50:17 -0400

    added webpack plugins for service worker and html webpack; implemented css loaders and babel to webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,12 +18,38 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // plugin to generate the html file
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "DAH TEXT EDITOR"
+      }),
+
+      // plugin for the custom service worker
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: 'src-sw.js'
+      })
     ],
 
     module: {
+      // CSS loaders
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
