8a0b2353f79905a1bef66eef124f31c1effc5818
rhonvyramos <rhonvyramos@gmail.com>
2023-10-01 09:01:48 -0400

    project startercode uploaded to git

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,30 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => console.error('putDb not implemented');
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log("GET from database");
+  const contactDb = await openDB('contact', 1);
+  const tx = contactDb.transaction('contact', 'readonly');
+  const store = tx.objectStore('contact');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
+initdb();
